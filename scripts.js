9d29d4d82d518ef26ef0141bf95f659a92305106
SuporteFDS <160695837+SuporteFDS@users.noreply.github.com>
2024-02-25 18:55:09 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -98,5 +98,5 @@ function salvarAnotacoes() {
 var card = document.getElementById('tns').value;
 
     card.addEventListener('click', () => {
-        "window.open('https://backoffice.tnscasino.com:8443/login', '_blank')">Site</button></br>
-}
+        window.open('https://backoffice.tnscasino.com:8443/login', '_blank');
+});
